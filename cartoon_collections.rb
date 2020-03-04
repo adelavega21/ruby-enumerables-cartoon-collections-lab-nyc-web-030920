efd6c8b15405c7baa92af5b26de4d932b8a38824
Andrew de la Vega <andrew21delavega@gmail.com>
2020-03-04 18:12:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = [
+  Doc,
+  Dopey,
+  Bashful,
+  Grumpy,
+  Sneezy,
+  Sleepy,
+  Happy
+]
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+roll_call_dwarves(dwarves)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
