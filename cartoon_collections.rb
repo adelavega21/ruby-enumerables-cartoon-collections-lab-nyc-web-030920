9113a5535c0019bf7d5ad53e5e51093a9dc3341a
Andrew de la Vega <andrew21delavega@gmail.com>
2020-03-04 18:18:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,16 +8,6 @@ dwarves = %w[
   Happy
 ]
 
-# Roll call for the next mining shift!
-#
-# Print out the following list using `puts`:
-# 1. Doc
-# 2. Dopey
-# 3. Bashful
-# etc. for all the dwarves
-#
-# Use the "each_with_index" method:
-# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-each_with_index
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
